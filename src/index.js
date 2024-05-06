0ca79a34f9b78572666c402b9c7673570786b2e7
lisamelina24 <131612347+lisamelina24@users.noreply.github.com>
2024-05-06 14:36:08 -0400

    Update API

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "54c1e6fa539tc26ef13o7a7b9b03e7b2";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
